746bd6743611992f3bbff80851d182ac079718c7
Joshua Jourdain <yahwehprovides@Joshuas-iMac.local>
2022-05-10 12:58:45 -0400

    add form to request-clean page; create request clean page

---
+++ pages/request-clean.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface IRequestCleanProps {
+}
+
+export default function RequestClean() {
+    return (
+        <div>
+
+            <iframe className="w-full aspect-video md:aspect-square" src="https://docs.google.com/forms/d/e/1FAIpQLSe2tEYR7GHqD_33wqKG5fb_ya7vKE7wvN8boF1SDYd6pIQC5A/viewform?embedded=true" width="640" height="1390" frameBorder="0" marginHeight="0" marginwidth="0">Loading…</iframe>
+
+        </div>
+    );
+}
