56fce957829cee39494e9ba207481a062324af92
Joshua Jourdain <yahwehprovides@Joshuas-iMac.local>
2022-05-10 13:01:00 -0400

    fix error; attempt repush to vercel

--- pages/request-clean.tsx
+++ pages/request-clean.tsx
@@ -7,7 +7,7 @@ export default function RequestClean() {
     return (
         <div>
 
-            <iframe className="w-full aspect-video md:aspect-square" src="https://docs.google.com/forms/d/e/1FAIpQLSe2tEYR7GHqD_33wqKG5fb_ya7vKE7wvN8boF1SDYd6pIQC5A/viewform?embedded=true" width="640" height="1390" frameBorder="0" marginHeight="0" marginwidth="0">Loading…</iframe>
+            <iframe className="w-full aspect-video md:aspect-square" src="https://docs.google.com/forms/d/e/1FAIpQLSe2tEYR7GHqD_33wqKG5fb_ya7vKE7wvN8boF1SDYd6pIQC5A/viewform?embedded=true" width="640" height="1390" frameBorder="0" >Loading…</iframe>
 
         </div>
     );
