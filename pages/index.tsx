e686e7376264b287cf7bac2d1d8c8d830bee7679
Joshua Jourdain <yahwehprovides@Joshuas-iMac.local>
2022-07-05 16:18:17 -0400

    add big h2 to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <LandingPageHeader />
 
 
-      <h1>we can't wait to hear from you</h1>
+      <h2 className="text-[100px] leading-[125px] md:text-[150px] md:leading-[175px] lg:text-[200px] lg:leading-[225px]  text-richBlue font-bold tracking-tight aquino">we work <br />with you</h2>
 
 
 
